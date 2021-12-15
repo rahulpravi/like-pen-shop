e5f04cf2485c92ac5118ce57b0f23e7da1a5fb62
ARgallery <rahulpravi14@gmail.com>
2021-12-15 07:52:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function js1(){
+  document.getElementById('img1').src="img_pen1.jpg";
+}
+function js2(){
+  document.getElementById('img1').src="img_pen2.jpg";
+}
+function js3(){
+  document.getElementById('img1').src="img_pen3.jpg";
+}
+function js4(){
+  document.getElementById('img1').src="img_pen4.jpg";
+}
